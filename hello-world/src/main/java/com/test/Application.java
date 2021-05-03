b2ed73ed1cec42ce187fd841b627f036a0a1b68f
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-03 13:53:45 +0200

    + dataSource

--- hello-world/src/main/java/com/test/Application.java
+++ hello-world/src/main/java/com/test/Application.java
@@ -1,13 +1,24 @@
 package com.test;
 
 import com.test.service.ItemService;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan// ("com.test")
 @Configuration
 public class Application {
+
+    @Bean
+    public HikariDataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:hsqldb:hsql://localhost/eshop");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext
