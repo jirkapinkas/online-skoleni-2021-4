e8981a66b599b40645ae1eff40aa84e20e6ec6e4
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-04 11:17:21 +0200

    + informace o tom, co v sobe schovava @SpringBootApplication

--- eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
+++ eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
@@ -1,9 +1,16 @@
 package com.example.eshopweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+// Anotace @SpringBootApplication v sobe schovava:
+//@ComponentScan
+//@Configuration
+//@EnableAutoConfiguration
 public class EshopwebApplication {
 
     public static void main(String[] args) {
