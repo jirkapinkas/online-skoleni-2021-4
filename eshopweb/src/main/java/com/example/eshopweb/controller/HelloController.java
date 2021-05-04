122766105943439070923f4c03d3a341774497bd
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-04 12:28:45 +0200

    Priklad na ResponseEntity

--- eshopweb/src/main/java/com/example/eshopweb/controller/HelloController.java
+++ eshopweb/src/main/java/com/example/eshopweb/controller/HelloController.java
@@ -1,11 +1,10 @@
 package com.example.eshopweb.controller;
 
 import com.example.eshopweb.pojo.Message;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,15 +13,36 @@ import java.nio.file.Path;
 @RestController
 public class HelloController {
 
+    // http://localhost:8080/hello
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello")
     @GetMapping("/hello")
     public Message message() {
         return new Message("Spring Boot Works! :-)");
     }
 
+    // http://localhost:8080/dog
     @GetMapping(path = "/dog", produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] dog() throws IOException {
         return Files.readAllBytes(Path.of("files","dog.jpg"));
+    }
+
+    // http://localhost:8080/file?name=dog.jpg
+    // http://localhost:8080/file?name=pom.xml
+    @GetMapping("/file")
+    public ResponseEntity<Object> file(@RequestParam String name) throws IOException {
+        Path path = Path.of("files", name);
+        MediaType mediaType;
+        if(name.endsWith(".jpg")) {
+            mediaType = MediaType.IMAGE_JPEG;
+        } else if(name.endsWith(".xml")) {
+            mediaType = MediaType.APPLICATION_XML;
+        } else {
+            mediaType = MediaType.TEXT_HTML;
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(mediaType)
+                .body(Files.readAllBytes(path));
     }
 
 }
