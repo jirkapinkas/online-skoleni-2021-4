bcd0a6b91e4b170abbf1634b38fa000f5ac611f0
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-04 11:29:10 +0200

    priklad na vraceni binarnich dat (vraci se obrazek pejska) ze serveru

--- eshopweb/src/main/java/com/example/eshopweb/controller/HelloController.java
+++ eshopweb/src/main/java/com/example/eshopweb/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.eshopweb.controller;
 
 import com.example.eshopweb.pojo.Message;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 public class HelloController {
@@ -13,6 +18,11 @@ public class HelloController {
     @GetMapping("/hello")
     public Message message() {
         return new Message("Spring Boot Works! :-)");
+    }
+
+    @GetMapping(path = "/dog", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] dog() throws IOException {
+        return Files.readAllBytes(Path.of("files","dog.jpg"));
     }
 
 }
