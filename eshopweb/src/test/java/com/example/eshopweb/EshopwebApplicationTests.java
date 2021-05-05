21c1c07746880f6552e6676e2ecdb6bef4971f1b
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-05 13:23:41 +0200

    + TestRestTemplate

--- eshopweb/src/test/java/com/example/eshopweb/EshopwebApplicationTests.java
+++ eshopweb/src/test/java/com/example/eshopweb/EshopwebApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.eshopweb;
 
+import com.example.eshopweb.dto.ItemDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EshopwebApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     void contextLoads() {
+        ItemDto itemDto = testRestTemplate.getForObject("/item/1", ItemDto.class);
+        // https://assertj.github.io/doc/
+        assertThat(itemDto)
+                .isNotNull();
+        assertThat(itemDto.getId())
+                .isEqualTo(1);
+
+        // https://www.testcontainers.org/
     }
 
 }
