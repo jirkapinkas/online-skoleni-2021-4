7adcac05dae50779f7460cb4631843970b2e4985
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-06 10:16:26 +0200

    Hibernate: Vysvetleni Managed, Detached & FLUSH

--- eshopweb/src/main/java/com/example/eshopweb/service/ItemService.java
+++ eshopweb/src/main/java/com/example/eshopweb/service/ItemService.java
@@ -7,7 +7,9 @@ import com.example.eshopweb.mapper.ItemMapper;
 import com.example.eshopweb.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +22,38 @@ public class ItemService {
 
     @Autowired
     private ItemMapper itemMapper;
+
+    @Transactional
+    public void exampleSaveManaged() {
+        Item item = new Item(); // entita je ve stavu "New"
+        // "Managed" = uvnitr aktivni transakce ziskame zaznam z databaze / ulozime zaznam do databaze => vysledny zaznam je ve stavu "Managed"
+        Item managedItem = itemRepository.save(item); // entita je ve stavu "Managed"
+        managedItem.setName("Stuff"); // Predtim nez se ukonci transakce, tak Hibernate vygeneruje UPDATE a nastavi stav entity
+        // TADY NEMUSIME VOLAT itemRepository.save(managedItem) ABY SE PROVEDL UPDATE ENTITY!!!
+    }
+
+    public void exampleSaveDetached() {
+        Item item = new Item(); // entita je ve stavu "New"
+        // "Detached" = ziskame zaznam z databaze / ulozime zaznam do databaze, ALE NEJSME UVNITR AKTIVNI TRANSAKCE => vysledny zaznam je ve stavu "Detached"
+        Item detachedItem = itemRepository.save(item); // entita je ve stavu "Detached"
+        detachedItem.setName("Stuff"); // Zmenili jsme stav objektu, TATO ZMENA SE NEPROPISE DO DATABAZE!!!
+        // POKUD CHCEME PROVEST UPDATE ENTITY, TAK TADY MUSIME ZAVOLAT itemRepository.save(detachedItem) ABY SE PROVEDL UPDATE ENTITY!!!
+    }
+
+    @Transactional
+    public void exampleSaveAndFlush() {
+        Item item = new Item(); // entita je ve stavu "New"
+        // "Managed" = uvnitr aktivni transakce ziskame zaznam z databaze / ulozime zaznam do databaze => vysledny zaznam je ve stavu "Managed"
+        // POZOR!!! Operace save() negarantuje, ze ihned provede INSERT / UPDATE!!! (obdobne metoda delete() negaranguje okamzite provedeni DELETE)
+        // Operace INSERT / UPDATE / DELETE generovane Hibernaty se provadeji kdyz se provede FLUSH
+        // Je garantovane, ze se FLUSH provede pred COMMITem (pred ukoncenim transakce)
+        // Nebo kdyz to ma vyznam kvuli konzistenci dat
+        // Nebo kdyz zavolate saveAndFlush(item)
+        Item managedItem = itemRepository.save(item); // entita je ve stavu "Managed"
+        managedItem.setName("Stuff"); // Predtim nez se ukonci transakce, tak Hibernate vygeneruje UPDATE a nastavi stav entity
+        // TADY NEMUSIME VOLAT itemRepository.save(managedItem) ABY SE PROVEDL UPDATE ENTITY!!!
+    }
+
 
     public List<ItemDto> findAll(String orderBy, String direction) {
         Sort sort = Sort.by(Sort.Direction.fromString(direction.toUpperCase()), orderBy);
