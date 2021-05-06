09b267b7b484e34cb0a7a6ced9240da3defd75d5
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-06 15:51:54 +0200

    + integracni testy

--- eshopweb/src/test/java/com/example/eshopweb/EshopwebApplicationTests.java
+++ eshopweb/src/test/java/com/example/eshopweb/EshopwebApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.eshopweb;
 
 import com.example.eshopweb.dto.ItemDto;
+import com.example.eshopweb.service.ItemService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,8 +21,16 @@ class EshopwebApplicationTests {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private ItemService itemService;
+
     @Test
     void contextLoads() {
+        List<ItemDto> items = itemService.findAll("id", "asc");
+        assertThat(items)
+                .isNotNull()
+                .isNotEmpty();
+
         ItemDto itemDto = testRestTemplate.getForObject("/item/1", ItemDto.class);
         // https://assertj.github.io/doc/
         assertThat(itemDto)
