c7dc04b3d56d5665a7b9540c9ea77ca2a97fd254
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-05-06 13:27:40 +0200

    Priklad na proxy (asynchronni posilani emailu)

--- eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
+++ eshopweb/src/main/java/com/example/eshopweb/EshopwebApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 // Anotace @SpringBootApplication v sobe schovava:
 //@ComponentScan
